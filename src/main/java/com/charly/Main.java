ceca6eb0899385f89a5c5b2cb1f373c3b7469359
charles <charles@example.com>
2026-07-25 00:55:32 +0300

    Add first Java variables examples

--- src/main/java/com/charly/Main.java
+++ src/main/java/com/charly/Main.java
@@ -1,17 +1,15 @@
 package com.charly;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String name = "Charles";
+        int age = 28;
+        double  height = 1.78;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
     }
 }
