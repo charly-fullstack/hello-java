80499baa9da257f0e54c79f4475f9c119b684fd4
charles <charles@example.com>
2026-07-26 02:39:10 +0300

    Complete Java primitive data types examples

--- src/main/java/com/charly/Main.java
+++ src/main/java/com/charly/Main.java
@@ -5,11 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         String name = "Charles";
-        int age = 28;
-        double  height = 1.78;
+        byte age = 25;
+        short year = 2025;
+        int number = 3000;
+        long worldPopulation = 8200000000L;
+        float temperature = 36.5f;
+        double height = 1.78;
+        char grade = 'A';
+        boolean isJavaFun = true;
         System.out.println(name);
         System.out.println(age);
+        System.out.println(year);
+        System.out.println(number);
+        System.out.println(worldPopulation);
+        System.out.println(temperature);
         System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isJavaFun);
 
     }
 }
