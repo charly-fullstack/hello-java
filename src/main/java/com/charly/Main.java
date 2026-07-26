2eb9138358dcd4f48c734d2a13f5c2610890c4e4
charles <charles@example.com>
2026-07-27 00:59:45 +0300

    Add Java arithmetic operators examples

--- src/main/java/com/charly/Main.java
+++ src/main/java/com/charly/Main.java
@@ -4,24 +4,14 @@ package com.charly;
 public class Main {
     public static void main(String[] args) {
 
-        String name = "Charles";
-        byte age = 25;
-        short year = 2025;
-        int number = 3000;
-        long worldPopulation = 8200000000L;
-        float temperature = 36.5f;
-        double height = 1.78;
-        char grade = 'A';
-        boolean isJavaFun = true;
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(year);
-        System.out.println(number);
-        System.out.println(worldPopulation);
-        System.out.println(temperature);
-        System.out.println(height);
-        System.out.println(grade);
-        System.out.println(isJavaFun);
+         int a = 20;
+         int b = 6;
+
+         System.out.println("Addition : " +(a + b));
+        System.out.println("Soustraction : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division : " + (a / b));
+        System.out.println("Modulo : " + (a % b));
 
     }
 }
