21d0ac78e9f44813f2d1c0d704e9a08968420724
charles <charles@example.com>
2026-08-02 01:56:14 +0300

    Rename ifExample to IfExaple

---
+++ src/main/java/com/charly/IfElseExample.java
@@ -0,0 +1,17 @@
+package com.charly;
+
+public class IfElseExample {
+
+    public static void main(String[] args) {
+
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        }else {
+            System.out.println("You are a minor.");
+        }
+
+        System.out.println("End of program");
+    }
+}
---
+++ src/main/java/com/charly/IfExample.java
@@ -0,0 +1,15 @@
+package com.charly;
+
+public class IfExample {
+
+    public static void main(String[] args) {
+
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        }
+
+        System.out.println("End of program.");
+    }
+}
