500a90a906fd827ccc4787c8ce8c85d839f889fb
charles <charles@example.com>
2026-08-04 01:36:17 +0300

    Add switch statement example

---
+++ src/main/java/com/charly/SwitchExample.java
@@ -0,0 +1,25 @@
+package com.charly;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+               System.out.println("Unknown day");
+        }
+
+        System.out.println("End of program.");
+    }
+}
