0487ecd51e5f072bd8cd5530fcc935c33fbbca27
charles <charles@example.com>
2026-08-30 16:00:10 +0300

    Remove unfinished nested loops exercise

--- src/main/java/com/charly/NestedLoops.java
+++ src/main/java/com/charly/NestedLoops.java
@@ -3,21 +3,6 @@ package com.charly;
 public class NestedLoops {
 
     public static void main(String[] args) {
-
-        int i = 1;
-        int j = 1;
-
-        for (i = 0; i < 10; i++) {
-            System.out.println(i);
-            i++;
-            for (j = 0; j < 12; j++) {
-                System.out.println(j);
-                j++;
-            }
-
-        }
-
-
     }
 
 }
