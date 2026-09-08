3c5fbaa9eec104fa6189cbcf0f90017507ea0bd7
charles <charles@example.com>
2026-09-09 02:28:53 +0300

    Add centered star pyramid with nested loops

--- src/main/java/com/charly/NestedLoops.java
+++ src/main/java/com/charly/NestedLoops.java
@@ -1,8 +1,18 @@
-package com.charly;
+    package com.charly;
 
-public class NestedLoops {
+    public class NestedLoops {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+
+            for (int i = 1; i <= 5; i++) { // contrôle les lignes
+                for (int j = 4; j >= i ; j--) { // contrôle le nombre d'espaces
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1 ; k++) { // contrôle le nombre d'étoiles
+                    System.out.print("*"); // affiche étoile
+                }
+
+                System.out.println(); // passe à la ligne suivante
+            }
+        }
     }
-
-}
